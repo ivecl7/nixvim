81b178b1dff4c6585e7024cd311c42bf3b84b928
ivecl7 <immortalrover@protonmail.com>
2025-08-09 09:16:31 +0800

    feat: Add some shortcuts for luasnip.
    Use <C-j/k> to jump between snippet-nodes.
    Use <C-n/p> to jump between choice-nodes.

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -87,6 +87,11 @@
           "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
         };
       };
+      luaConfig.post = ''
+        local cmp = require("cmp")
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      '';
     };
     cmp-nvim-lsp = {
       enable = true;
--- config/plugins/snippets/luasnip.nix
+++ config/plugins/snippets/luasnip.nix
@@ -1,9 +1,27 @@
-_: {
+{
   plugins.luasnip = {
     enable = true;
     settings = {
       enable_autosnippets = true;
       store_selection_keys = "<Tab>";
     };
+    luaConfig.post = ''
+      local ls = require("luasnip")
+
+      vim.keymap.set({ "i", "s" }, "<c-k>", function()
+        if ls.expand_or_jumpable() then
+          ls.expand_or_jump()
+        end
+      end, { silent = true })
+      vim.keymap.set({ "i", "s" }, "<c-j>", function()
+        if ls.jumpable(-1) then
+          ls.jump(-1)
+        end
+      end, { silent = true })
+      vim.api.nvim_set_keymap("i", "<C-n>", "<Plug>luasnip-next-choice", {})
+      vim.api.nvim_set_keymap("s", "<C-n>", "<Plug>luasnip-next-choice", {})
+      vim.api.nvim_set_keymap("i", "<C-p>", "<Plug>luasnip-prev-choice", {})
+      vim.api.nvim_set_keymap("s", "<C-p>", "<Plug>luasnip-prev-choice", {})
+    '';
   };
 }
