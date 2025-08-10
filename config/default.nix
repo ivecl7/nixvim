ed5d89933c1f90a1fd663de1062103828607f312
ivecl7 <immortalrover@protonmail.com>
2025-08-10 15:22:55 +0800

    feat: Add a new plugin marks.nvim.
    add a new plugin to add marks.

--- config/default.nix
+++ config/default.nix
@@ -28,6 +28,7 @@ _: {
     ./plugins/editor/todo-comments.nix
     ./plugins/editor/copilot-chat.nix
     ./plugins/editor/navic.nix
+    ./plugins/editor/marks.nix
 
     # UI plugins
     ./plugins/ui/bufferline.nix
