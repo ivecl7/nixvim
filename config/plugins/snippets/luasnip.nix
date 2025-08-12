eea8037322494dcc2056e8d05487cb439a8ba08a
ivecl7 <immortalrover@protonmail.com>
2025-08-12 14:36:28 +0800

    fix: Fix the luasnip jump shortcut key.
    Because the copilot completions usually mess up the jumpable points,
    set the key from <C-j/k> to <C-h/l>.

--- config/plugins/snippets/luasnip.nix
+++ config/plugins/snippets/luasnip.nix
@@ -13,12 +13,12 @@
     luaConfig.post = ''
       local ls = require("luasnip")
 
-      vim.keymap.set({ "i", "s" }, "<c-k>", function()
+      vim.keymap.set({ "i", "s" }, "<c-l>", function()
         if ls.expand_or_jumpable() then
           ls.expand_or_jump()
         end
       end, { silent = true })
-      vim.keymap.set({ "i", "s" }, "<c-j>", function()
+      vim.keymap.set({ "i", "s" }, "<c-h>", function()
         if ls.jumpable(-1) then
           ls.jump(-1)
         end
