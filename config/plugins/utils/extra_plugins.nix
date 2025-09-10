59566f95c3658e5a5e529801190851bda0c4acd0
ivecl7 <immortalrover@protonmail.com>
2025-09-10 12:18:13 +0800

    feat: add a new plugin fcitx.nvim
    It's used for writing chinese. You need to ensure you have fcitx in ypur
    PATH.

--- config/plugins/utils/extra_plugins.nix
+++ config/plugins/utils/extra_plugins.nix
@@ -1,6 +1,20 @@
 { pkgs, ... }:
+let
+  fcitx = pkgs.vimUtils.buildVimPlugin {
+    name = "fcitx";
+    src = pkgs.fetchFromGitHub {
+      owner = "h-hg";
+      repo = "fcitx.nvim";
+      rev = "dcb6b70888aa893d3d223bb777d4117bbdac06a7";
+      hash = "sha256-Fa3BrXmDYF135Fv4FV+Zjv1mdgD9UdqON2FJxOHkkUI=";
+    };
+  };
+in
 {
-  extraPlugins = with pkgs.vimPlugins; [
-    plenary-nvim
-  ];
+  extraPlugins =
+    with pkgs.vimPlugins;
+    [
+      plenary-nvim
+    ]
+    ++ [ fcitx ];
 }
